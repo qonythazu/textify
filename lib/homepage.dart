a4252d652c048460ffa6449343f3fd084ffc0f94
qonythazu <qonitaarif5@gmail.com>
2023-05-02 18:37:14 +0800

    au ah munyak

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:async';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 import 'package:native_pdf_renderer/native_pdf_renderer.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -49,15 +52,28 @@ class _PdfToImageConverterState extends State<PdfToImageConverter> {
       for (int i = 1; i <= document.pagesCount; i++) {
         final page = await document.getPage(i);
 
+        // Calculate aspect ratio of PDF page
+        final aspectRatio = page.width / page.height;
+
+        // Calculate desired image size (e.g. 800 x 600)
+        final desiredWidth = 800;
+        final desiredHeight = (desiredWidth / aspectRatio).round();
+
+        // Render PDF page to image
         final pageImage = await page.render(
-          width: page.width,
-          height: page.height,
+          width: desiredWidth.toDouble(),
+          height: desiredHeight.toDouble(),
         );
 
-        final tempDir = await getTemporaryDirectory();
-        final imagePath = '${tempDir.path}/temp.jpg';
+        // Crop image to desired aspect ratio
+        final cropRect = Rect.fromLTWH(0, 0, desiredWidth.toDouble(), desiredHeight.toDouble());
+        final croppedImage = await cropImage(pageImage!.bytes, cropRect, desiredWidth.toDouble(), desiredHeight.toDouble());
 
-        final imageFile = await File(imagePath).writeAsBytes(pageImage!.bytes);
+        // Save image to temporary directory
+        final tempDir = await getTemporaryDirectory();
+        final imagePath = '${tempDir.path}/image.png';
+
+        final imageFile = await File(imagePath).writeAsBytes(croppedImage);
 
         _pages.add(File(imagePath).readAsBytesSync());
 
@@ -76,12 +92,40 @@ class _PdfToImageConverterState extends State<PdfToImageConverter> {
     }
   }
 
+  Future<Uint8List> cropImage(Uint8List imageBytes, Rect cropRect, double width, double height) async {
+    final completer = Completer<Uint8List>();
+    final decoder = await ui.instantiateImageCodec(imageBytes);
+    final frame = await decoder.getNextFrame();
+    final croppedImage = await frame.image.toByteData(format: ui.ImageByteFormat.png);
+
+    final croppedBytes = await croppedImage!.buffer.asUint8List();
+    final croppedImageWidget = Image.memory(croppedBytes);
+    final cropKey = GlobalKey();
+    final cropWidget = ClipRect(
+      child: OverflowBox(
+        maxWidth: double.infinity,
+        maxHeight: double.infinity,
+        alignment: Alignment.center,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: croppedImageWidget,
+        ),
+      ),
+    );
+    final boundary = cropKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    final image = await boundary.toImage(pixelRatio: 1.0);
+    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    completer.complete(byteData!.buffer.asUint8List());
+    return completer.future;
+  }
+
 
   // Upload gambar ke node js
   Future<void> _uploadImage(Uint8List imageBytes) async {
     final request = http.MultipartRequest(
       'POST',
-      Uri.parse('http://localhost:3000/upload'),
+      Uri.parse('http://192.168.1.20/upload'),
     );
     request.files.add(http.MultipartFile.fromBytes(
       'image',
@@ -112,6 +156,11 @@ class _PdfToImageConverterState extends State<PdfToImageConverter> {
           ),
         ),
       ),
+      floatingActionButton: _pages.isNotEmpty ? null : FloatingActionButton(
+        onPressed: _pickPDF,
+        tooltip: 'Pick PDF',
+        child: const Icon(Icons.file_upload, color: Colors.white),
+      ),
       body: _isLoading
           ? const Center(
               child: CircularProgressIndicator(),
@@ -125,12 +174,21 @@ class _PdfToImageConverterState extends State<PdfToImageConverter> {
                     ),
                   ),
                 )
-              : const SizedBox(),
-      floatingActionButton: _pages.isNotEmpty ? null : FloatingActionButton(
-        onPressed: _pickPDF,
-        tooltip: 'Pick PDF',
-        child: const Icon(Icons.file_upload, color: Colors.white),
-      ),
+              : GridView.builder(
+                  padding: const EdgeInsets.all(10),
+                  itemCount: _pages.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.memory(
+                      _pages[index],
+                      fit: BoxFit.cover,
+                    );
+                  },
+                ),
     );
   }
 }
